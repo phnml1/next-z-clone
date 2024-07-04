e2865cdb4f129963cb1157ed76f74f4db6deda88
lee juyoung <juyung0903@naver.com>
2024-07-04 23:17:11 +0900

    ch4: search 부분 URISearchParams 적용

--- src/app/(afterLogin)/search/_component/SearchResult.tsx
+++ src/app/(afterLogin)/search/_component/SearchResult.tsx
@@ -2,14 +2,13 @@
 
 import Post from "@/app/(afterLogin)/_component/Post";
 import { Post as IPost } from '@/model/Post';
-import {getSearchResult} from "../_lib/getSearchResult";
+import {getSearchResult} from "@/app/(afterLogin)/search/_lib/getSearchResult";
 import {useQuery} from "@tanstack/react-query";
 
 type Props = {
   searchParams: { q: string, f?: string, pf?: string };
 }
-// cache time을 짧게해서 메모리에 저장된 검색결과 정리해주는게 좋음
-export default function SearchResult({ searchParams}: Props) {
+export default function SearchResult({ searchParams }: Props) {
   const {data} = useQuery<IPost[], Object, IPost[], [_1: string, _2: string, Props['searchParams']]>({
     queryKey: ["posts", "search", searchParams],
     queryFn: getSearchResult,
